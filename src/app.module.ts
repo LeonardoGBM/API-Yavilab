cb93fb3245fc287d7d95d394e9125a9c120e2fb2
LeonardoGBM <leonardog.mh@outlook.com>
2024-07-28 03:16:17 -0500

    cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LaboratoryModule } from './laboratory/laboratory.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LaboratoryEntity } from './laboratory/laboratory.entity';
 
 
 @Module({
-  imports: [LaboratoryModule],
+  imports: [ LaboratoryModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'yavilab',
+      entities: [LaboratoryEntity],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
